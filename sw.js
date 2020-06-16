d0bdc2902ed81b160308012b4bd300c56ba3f42e
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2020-06-16 18:40:57 +0100

    added shortcuts

--- sw.js
+++ sw.js
@@ -1,6 +1,40 @@
 const CACHE = "PasswordGen";
-const precacheFiles = ["/passwordgen/","/passwordgen/index.html","/passwordgen/advanced.html","/passwordgen/check.html",
+const precacheFiles = ["/passwordgen/","/passwordgen/index.html","/passwordgen/advanced.html","/passwordgen/check.html","/passwordgen/index.html","/passwordgen/advanced.html","/passwordgen/check.html",
 ];
+
+/**
+ * Check to see if the request is in the cache
+ *
+ * Return response
+ *
+ * If not in the cache, then return
+ *
+ * @param {RequestInfo} request
+ * @return {Promise} promise
+ */
+function fromCache(request) {
+	return caches.open(CACHE).then(function(cache) {
+		return cache.match(request).then(function(matching) {
+			if (!matching || matching.status === 404) {
+				return Promise.reject("no-match");
+			}
+			return matching;
+		});
+	});
+}
+
+/**
+ * Update the cache
+ * @param {RequestInfo} request
+ * @param {Response} response
+ * @return {Promise} promise
+ */
+function updateCache(request, response) {
+	return caches.open(CACHE).then(function(cache) {
+		return cache.put(request, response);
+	});
+}
+
 
 self.addEventListener("install", function(event) {
 	self.skipWaiting();
@@ -16,13 +50,18 @@ self.addEventListener("activate", function(event) {
 	event.waitUntil(self.clients.claim());
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
+
+// If any fetch fails, it will look for the request in the cache and serve it
+// from there first
 self.addEventListener("fetch", function(event) {
-	if (event.request.method !== "GET") return;
+	if (event.request.method !== "GET") {
+		return;
+	}
 	event.respondWith(
 		fromCache(event.request).then(
 			function(response) {
-				// The response was found in the cache so we respond with it and update the entry
+				// The response was found in the cache so we respond with it and
+				// update the entry
 				// This is where we call the server to get the newest version of the
 				// file to use the next time we show view
 				event.waitUntil(
@@ -33,7 +72,8 @@ self.addEventListener("fetch", function(event) {
 				return response;
 			},
 			function() {
-				// The response was not found in the cache so we look for it on the server
+				// The response was not found in the cache so we look for it on
+				// the server
 				return fetch(event.request)
 					.then(function(response) {
 						// If request was success, add or update it in the cache
@@ -46,23 +86,3 @@ self.addEventListener("fetch", function(event) {
 		),
 	);
 });
-
-function fromCache(request) {
-	// Check to see if you have it in the cache
-	// Return response
-	// If not in the cache, then return
-	return caches.open(CACHE).then(function(cache) {
-		return cache.match(request).then(function(matching) {
-			if (!matching || matching.status === 404) {
-				return Promise.reject("no-match");
-			}
-			return matching;
-		});
-	});
-}
-
-function updateCache(request, response) {
-	return caches.open(CACHE).then(function(cache) {
-		return cache.put(request, response);
-	});
-}
