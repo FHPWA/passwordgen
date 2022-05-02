9e108fcf99253a30818a76f81a3263babeb0975b
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2022-05-02 10:22:22 +0100

    filter png images

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,12 @@
 module.exports = function (config) {
 	config.addTransform("minify", require("./transforms/minify"))
-	config.addPassthroughCopy("./src/images")
 	config.addPassthroughCopy("./src/resources")
 	return {
 		passthroughFileCopy: true,
 		dataTemplateEngine: "njk",
 		htmlTemplateEngine: "njk",
 		markdownTemplateEngine: "njk",
-		templateFormats: ["html", "css", "js", "njk", "md"],
+		templateFormats: ["html", "css", "js", "njk", "md", "png"],
 		dir: {
 			input: "src",
 			output: "docs",
